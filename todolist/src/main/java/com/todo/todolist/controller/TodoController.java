70a31e814840ffdbe4633e429e8b1bc45a27d7d1
Dayanne Souza <dadaysmo@gmail.com>
2025-07-02 02:05:12 -0300

    feat: error handling

--- todolist/src/main/java/com/todo/todolist/controller/TodoController.java
+++ todolist/src/main/java/com/todo/todolist/controller/TodoController.java
@@ -2,6 +2,7 @@ package com.todo.todolist.controller;
 
 import com.todo.todolist.entity.Todo;
 import com.todo.todolist.repository.TodoRepository;
+import com.todo.todolist.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
@@ -27,39 +28,30 @@ public class TodoController {
     
     @PutMapping ("/{id}")
     public ResponseEntity<Todo> atualizarTodo(@PathVariable Long id, @RequestBody Todo todoDetalhes){
-    	//busca tarefa no BD
-    	Optional<Todo> optionalTodo = todoRepository.findById(id);
-    	
-    	//verifica se encontrada
-    	if (optionalTodo.isPresent()) {
-    		// pega o objeto
-    		Todo todoExistente = optionalTodo.get();
+     	
+    	//verifica se encontrada a tarefa no BD
+    	Todo todoExistente = todoRepository.findById(id)
+    			.orElseThrow(() -> new ResourceNotFoundException("Tarefa com o ID " + id + " não foi encontrada"));
     		
-    		//att campos
-    		todoExistente.setTitulo(todoDetalhes.getTitulo());
-    		todoExistente.setDescricao(todoDetalhes.getDescricao());
-    		todoExistente.setFeito(todoDetalhes.isFeito());
+    	//att campos
+    	todoExistente.setTitulo(todoDetalhes.getTitulo());
+    	todoExistente.setDescricao(todoDetalhes.getDescricao());
+    	todoExistente.setFeito(todoDetalhes.isFeito());
     		
-    		//salva att e retorna ok
-    		return ResponseEntity.ok(todoRepository.save(todoExistente));
-    	} else {
-    		//se não, retorna 404 not found
-    		return ResponseEntity.notFound().build();
-    	}
-    }
+    	//salva att e retorna ok
+    	return ResponseEntity.ok(todoRepository.save(todoExistente));
+   }
     
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarTodo(@PathVariable Long id) {
     	
     	//verifica se tarefa existe
-    	if (todoRepository.existsById(id)) {
-    		//se existe = deleta
-    		todoRepository.deleteById(id);
-    		//retorna sucesso, sem conteúdo
-    		return ResponseEntity.noContent().build();
-    	} else {
-    		//se não existe, retorna 404 not found
-    		return ResponseEntity.notFound().build();
+    	if (!todoRepository.existsById(id)) {
+    		throw new ResourceNotFoundException("Tarefa com o ID " + id + " não foi encontrada");
     	}
+    		
+    		//retorna sucesso, sem conteúdo
+    	todoRepository.deleteById(id);
+    	return ResponseEntity.noContent().build();
     }
 }
